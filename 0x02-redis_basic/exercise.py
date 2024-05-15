96f022a108357804d8952d5ff9c05dfbfb9b8e5c
Simo Khanyi <simokhanyi@gmail.com>
2024-05-15 20:49:36 +0000

    Redis basics

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,17 +9,26 @@ from typing import Callable, Optional, Union
 
 
 class Cache:
+    """Cache class for storing and retrieving data with Redis."""
+
     def __init__(self):
+        """Initialize the Cache instance and flush the Redis database."""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: 'Union[str, bytes, int, float]') -> str:
+        """Store data in Redis and return a unique key for retrieval."""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
     def get(self, key: str, fn: Optional[Callable] = None) -> Union[
             str, bytes, int, float]:
+        """Retrieve data from Redis using the provided key.
+
+        Optionally apply a conversion function (fn) to the retrieved data.
+
+        """
         data = self._redis.get(key)
         if data is None:
             return None
@@ -28,12 +37,15 @@ class Cache:
         return data
 
     def get_str(self, key: str) -> str:
+        """Retrieve data from Redis and decode it as a UTF-8 string."""
         return self.get(key, fn=lambda d: d.decode("utf-8"))
 
     def get_int(self, key: str) -> int:
+        """Retrieve data from Redis and convert it to an integer."""
         return self.get(key, fn=int)
 
     def count_calls(method: Callable) -> Callable:
+        """Decorator to count the number of times a method is called."""
         key = method.__qualname__
 
         @functools.wraps(method)
@@ -44,6 +56,7 @@ class Cache:
         return wrapper
 
     def call_history(method: Callable) -> Callable:
+        """Decorator to track the inputs and outputs of a method."""
         input_key = f"{method.__qualname__}:inputs"
         output_key = f"{method.__qualname__}:outputs"
 
@@ -59,12 +72,14 @@ class Cache:
     @count_calls
     @call_history
     def store(self, data: 'Union[str, bytes, int, float]') -> str:
+        """Store data in Redis, count the call, and track the history."""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
     @staticmethod
     def replay(func: Callable):
+        """Replay the history of inputs and outputs for a method."""
         input_key = f"{func.__qualname__}:inputs"
         output_key = f"{func.__qualname__}:outputs"
 
